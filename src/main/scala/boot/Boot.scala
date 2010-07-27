74928ea22dbf6ba613b4f9d78d3063ade8ddc2ec
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2010-07-27 08:36:49 +0200

    added Boot class and config for later

---
+++ src/main/scala/boot/Boot.scala
@@ -0,0 +1,10 @@
+package boot
+
+import se.scalablesolutions.akka.actor.Actor._
+
+/**
+ * Boot class. not implemented yet.
+ */
+private[boot] class Boot {
+  // boot will be done here
+}
--- src/test/scala/unit/akka/SimpleActorTest.scala
+++
@@ -1,6 +0,0 @@
-package unit.akka
-
-class SimpleActorTest extends {
-
-
-}
