bde4b5fbd3178855a5d238c91a094f1d1f0cfcdb
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2010-09-24 08:58:18 +0200

    added  simple remote actor test

--- src/test/scala/unit/akka/RemoteClientTest.scala
+++ src/test/scala/unit/akka/RemoteClientTest.scala
@@ -6,13 +6,15 @@ import org.scalatest.matchers.ShouldMatchers
 import se.scalablesolutions.akka.remote.{RemoteClient, RemoteServer}
 import se.scalablesolutions.akka.actor.Actor._
 import se.scalablesolutions.akka.actor.Actor
+import java.util.concurrent.CyclicBarrier
 
 class RemoteClientTest extends Spec with ShouldMatchers with BeforeAndAfterAll with Logging {
   val server = new RemoteServer
+  val barrier = new CyclicBarrier(2)
 
   override def beforeAll(configMap: Map[String, Any]) {
     server.start("localhost", 11000)
-    server.register("test", actorOf(new TestActor))
+    server.register("test", actorOf(new TestActor(barrier)))
   }
 
   override def afterAll(configMap: Map[String, Any]) {
@@ -29,21 +31,22 @@ class RemoteClientTest extends Spec with ShouldMatchers with BeforeAndAfterAll w
     it("should not get exceptions") {
       val ref = RemoteClient.actorFor("test", "localhost", 11000)
       ref ! "hello"
-      Thread.sleep(1000)
+      barrier.await
       val reply = ref !! "hello"
       reply match {
-        case Some(msg:String) => msg should be ("hello")
+        case Some(msg: String) => msg should be("hello")
         case None => fail("no reply")
       }
     }
   }
 
 }
-class TestActor extends Actor {
+class TestActor(barrier:CyclicBarrier) extends Actor {
   def receive = {
     case msg: Any => {
-        log.info("msg: %s",msg.toString)
-        self.reply_?(msg)
+      log.info("msg: %s", msg.toString)
+      self.reply_?(msg)
+      barrier.await
     }
   }
 }
