5c6a35bbb5dff5575144c2fadf49072a00be3a1c
Raymond Roestenburg <rroestenburg@csc.com>
2010-11-29 17:02:41 +0100

    added AkkaRepo so akka-sbt-plugin 1.0-RC1 can be resolved

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,20 +2,20 @@ import sbt._
 import Process._
 import sbt_akka_bivy._
 class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject with AkkaKernelDeployment{
+   val AkkaRepo = "Akka Repository" at "http://scalablesolutions.se/akka/repository"
   val akkaCamel = akkaModule("camel")
   val akkaKernel = akkaModule("kernel")
   val akkaRedis = akkaModule("persistence-redis")
   val junit = "junit" % "junit" % "4.8.1" % "test->default"
-  val camelFtp= "org.apache.camel" % "camel-ftp" % "2.4.0" % "compile"
-  val camelMina= "org.apache.camel" % "camel-mina" % "2.4.0" % "compile"
-  val camelJetty= "org.apache.camel" % "camel-jetty" % "2.4.0" % "compile"
+  val camelFtp= "org.apache.camel" % "camel-ftp" % "2.5.0" % "compile"
+  val camelMina= "org.apache.camel" % "camel-mina" % "2.5.0" % "compile"
+  val camelJetty= "org.apache.camel" % "camel-jetty" % "2.5.0" % "compile"
   // for coverage
   val emma = "emma" % "emma" % "2.0.5312" % "test->default"
   val jgroups = "jgroups" % "jgroups" % "2.9.0.GA" % "compile" 
   val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test->default"
-  override def repositories = Set( "scala-tools-snapshots" at 
-      "http://scala-tools.org/repo-snapshots/", "se.scalablesolutions"
-      at "http://www.scalablesolutions.se/akka/repository",
+  override def repositories = Set("scala-tools-snapshots" at 
+      "http://www.scalablesolutions.se/akka/repository",
       "jboss" at "http://repository.jboss.org/maven2"
   ) 
  
